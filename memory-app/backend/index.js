595f58ddf6187228e55324ee1f90cb432b702390
achraf_nait_belkacem <achrafoonb@gmail.com>
2024-12-28 23:50:17 +0100

    Update index.js

--- memory-app/backend/index.js
+++ memory-app/backend/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+//const express = require('express');
+//const bodyParser = require('body-parser');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
