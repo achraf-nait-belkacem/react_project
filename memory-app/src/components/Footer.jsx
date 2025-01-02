b315863e12b7e3c82d23ef1efa3e7993b8aaad04
achraf_nait_belkacem <achrafoonb@gmail.com>
2025-01-02 12:04:01 +0100

    Create Footer.jsx

---
+++ memory-app/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <p>
+        Powered by{' '}
+        <a 
+          href="https://github.com/achraf-nait-belkacem"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="author-name"
+        >
+          NAIT BELKACEM Achraf
+        </a>
+      </p>
+      <p className="copyright">© 2024-2025</p>
+    </footer>
+  );
+};
+
+export default Footer; 
